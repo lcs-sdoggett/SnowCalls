673f9cd9d95c8a943238a497b617b4cb490ce022
Scott Doggett <sdoggett@lcs.on.ca>
2019-11-27 21:00:26 -0500

    Initial Commit

---
+++ SnowCalls/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnowCalls
+//
+//  Created by Doggett, Scott on 2019-11-27.
+//  Copyright © 2019 Doggett, Scott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
