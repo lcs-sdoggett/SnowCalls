80f09b4b6e41fbe6c5bb2de6200903332f1757a1
Scott Doggett <sdoggett@lcs.on.ca>
2019-12-03 14:57:49 -0500

    fixed logic, works with strings that have multiple lines

--- SnowCalls/ViewController.swift
+++ SnowCalls/ViewController.swift
@@ -18,13 +18,14 @@ class ViewController: UIViewController {
     
     var numberOfCharacters = 0
     
-    //MARK: Functions
+    //MARK: Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
+    //MARK: Actions
     @IBAction func convert(_ sender: Any) {
         
         // Convert input to string
@@ -33,50 +34,59 @@ class ViewController: UIViewController {
             return
         }
         
-        // Create empty sting - will become the out
-        var outputString = ""
+        // Reset message output
+        messageOutput.text = ""
         
+        // Split up the inputed string
+        let phoneNumbers = messageInput.split(separator: "\n")
         
-        for singleCharacter in messageInput{
+        for phoneNumber in phoneNumbers {
             
-            switch singleCharacter.lowercased() {
-            case "0":
-                outputString += "0"
-            case "1":
-                outputString += "1"
-            case "2","a","b","c":
-                outputString += "2"
-            case "3","d","e","f":
-                outputString += "3"
-            case "4","g","h","i":
-                outputString += "4"
-            case "5","j","k","l":
-                outputString += "5"
-            case "6","m","n","o":
-                outputString += "6"
-            case "7","p","q","r","s":
-                outputString += "7"
-            case "8","t","u","v":
-                outputString += "8"
-            case "9","w","x","y","z":
-                outputString += "9"
-            case "\n":
-                outputString += "\n"
-            default:
-                outputString += ""
+            // Create empty sting - will become the out
+            var outputString = ""
+            
+            for singleCharacter in phoneNumber{
+                
+                switch singleCharacter.lowercased() {
+                case "0":
+                    outputString += "0"
+                case "1":
+                    outputString += "1"
+                case "2","a","b","c":
+                    outputString += "2"
+                case "3","d","e","f":
+                    outputString += "3"
+                case "4","g","h","i":
+                    outputString += "4"
+                case "5","j","k","l":
+                    outputString += "5"
+                case "6","m","n","o":
+                    outputString += "6"
+                case "7","p","q","r","s":
+                    outputString += "7"
+                case "8","t","u","v":
+                    outputString += "8"
+                case "9","w","x","y","z":
+                    outputString += "9"
+                case "-":
+                    outputString += ""
+                default:
+                    outputString += ""
+                }
+                
+                // Add a dash after 3 and 7 characters
+                if outputString.count == 3 || outputString.count == 7 {
+                    outputString += "-"
+                }
+                
+                if outputString.count == 12 {
+                    //End the loop
+                    break
+                }
+                
             }
             
-            if outputString.count == 3 || outputString.count == 7 {
-                outputString += "-"
-            }
-            
-            
+            messageOutput.text += outputString + "\n"
         }
-        
-        messageOutput.text = outputString
     }
-    
-    
-    
 }
-
